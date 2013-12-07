029d019ba4cb1f240deb1c1395d7adb96101064d
al1ensuu <462993145@qq.com>
2013-12-07 21:03:32 +0800

    alpha3

--- SimpCost/DetailViewController.m
+++ SimpCost/DetailViewController.m
@@ -9,6 +9,16 @@
 #import "DetailViewController.h"
 
 @interface DetailViewController ()
+{
+    NSMutableArray *values;
+    NSMutableArray *dates;
+    NSMutableArray *notes;
+}
+
+@property (strong, nonatomic) IBOutlet UILabel *value;
+@property (strong, nonatomic) IBOutlet UILabel *note;
+@property (strong, nonatomic) IBOutlet UILabel *time;
+@property (strong, nonatomic) IBOutlet UILabel *location;
 
 @end
 
@@ -27,6 +37,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    values = [[NSMutableArray alloc] initWithArray:[[NSUserDefaults standardUserDefaults] objectForKey:@"Values"]];
+    dates = [[NSMutableArray alloc] initWithArray:[[NSUserDefaults standardUserDefaults] objectForKey:@"Dates"]];
+    notes = [[NSMutableArray alloc] initWithArray:[[NSUserDefaults standardUserDefaults] objectForKey:@"Notes"]];
+    
+    NSInteger currentID = [[NSUserDefaults standardUserDefaults] integerForKey:@"CurrentDetailID"];
+    self.value.text = [NSString stringWithFormat:@"%@¥%@",[[values objectAtIndex:currentID] substringToIndex:2],[[values objectAtIndex:currentID] substringFromIndex:2]];
+    self.note.text = [notes objectAtIndex:currentID];
+    self.time.text = [dates objectAtIndex:currentID];
+}
+
+- (IBAction)back:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning
